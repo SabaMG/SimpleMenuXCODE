94c85f7eb4d7b756610b40d53b804649ae095288
Tristan Faure <tristan.faure@epita.fr>
2024-10-14 10:28:21 +0200

    Initial Commit

---
+++ Menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Menu
+//
+//  Created by Tristan Faure on 14/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Tristan Faure on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
